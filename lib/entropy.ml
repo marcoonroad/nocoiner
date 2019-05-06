7a0103d3a1b7d9d035eecefb75b914340d42b98f
Marco Aurélio da Silva <marcoonroad@gmail.com>
2019-05-05 23:19:22 -0300

    refactor: :recycle: increasing IV size from 128 bits to 160 bits now :open_mouth:
    Signed-off-by: Marco Aurélio da Silva <marcoonroad@gmail.com>

--- lib/entropy.ml
+++ lib/entropy.ml
@@ -3,6 +3,6 @@ module NumZ = Nocrypto.Numeric.Z
 
 let key () = NumZ.to_cstruct_be @@ RngZ.gen_bits 256
 
-let iv () = NumZ.to_cstruct_be @@ RngZ.gen_bits 128
+let iv () = NumZ.to_cstruct_be @@ RngZ.gen_bits 160
 
 let _ = Nocrypto_entropy_unix.initialize ()
